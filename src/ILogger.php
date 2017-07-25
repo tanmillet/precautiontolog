a4120066b9fef784e9bb673f63f4952ee7ae74ff
tanmillet <tanmillet@sina.com>
2017-07-25 11:49:10 +0800

    add PrecautionMessages logger to write read

---
+++ src/ILogger.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Terry Lucas
+ * Date: 2017/7/25
+ * Time: 11:33
+ */
+
+namespace TerryLucasInterFaceLog\Logger;
+
+
+interface ILogger
+{
+        public function write($method , array $options = []);
+        public function read();
+}
---
+++ src/Logger.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Terry Lucas
+ * Date: 2017/7/25
+ * Time: 11:32
+ */
+
+namespace TerryLucasInterFaceLog\Logger;
+
+use TerryLucasInterFaceLog\Logger\Concerns\PrecautionMessages;
+
+/**
+ * Class Logger
+ * User: Terry Lucas
+ * @package TerryLucasInterFaceLog\Logger
+ */
+class Logger implements ILogger
+{
+    use PrecautionMessages;
+
+    /**
+     * User: Terry Lucas
+     * @param $method
+     * @param array $options
+     * @throws \Exception
+     */
+    public function write($method, array $options = [])
+    {
+        try {
+
+        } catch (\Exception $e) {
+            throw  $e;
+        }
+    }
+
+    /**
+     * User: Terry Lucas
+     */
+    public function read()
+    {
+        // TODO: Implement read() method.
+    }
+}
